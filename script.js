0a820fab7bc2c9802e5ed048262173ab1ce6742d
caio-faustino <caioexfaustino.pro@gmail.com>
2022-09-01 17:28:23 -0300

    requisito 05

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-function button() {
-  const clickPaleta = document.querySelectorAll('.color');
-  const newButton = document.getElementById('button-random-color');
-  newButton.addEventListener('click', () => {
-    for (let index = 0; index < clickPaleta.length; index += 1) {
-      const cor1 = Math.floor(Math.random() * 255);
-      const cor2 = Math.floor(Math.random() * 255);
-      const cor3 = Math.floor(Math.random() * 255);
+const botaoCor = document.getElementById('button-random-color');
+botaoCor.addEventListener('click', colorRandomizer);
 
-      if (index === 0) {
-        clickPaleta[index].style.backgroundColor = 'black';
-      } else {
-        clickPaleta[index].style.backgroundColor = `rgb(${cor1}, ${cor2}, ${cor3})`;
-      }
-    }
-  });
+const colors = document.getElementsByClassName('color');
+let colorStorage = [];
+colorStorage[0] = '#000000';
+
+function colorRandomizer() {
+  
+  let randomColor;
+
+  for (let i = 1; i < colors.length; i += 1) {
+    randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+    colors[i].style.backgroundColor = `#${randomColor}`;
+    colorStorage[i] = `#${randomColor}`;
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(colorStorage));
 }
-button();
+
+function colorRestore() {
+  colorStorage = JSON.parse(localStorage.getItem('colorPalette'));
+
+  for (let j = 1; j < colors.length; j += 1) {
+    colors[j].style.backgroundColor = colorStorage[j];
+  }
+}
+
+function start() {
+  if (localStorage.getItem('colorPalette') === null) {
+    colorRandomizer();
+  } else {
+    colorRestore();
+  }
+}
+
+window.onload = start;
